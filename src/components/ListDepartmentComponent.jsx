e283b3dda1d375491fbf328c1868ba014afb3d14
RishiBhange <155911629+RishiBhange@users.noreply.github.com>
2026-04-12 18:34:25 +0530

    Update ListDepartmentComponent.jsx

--- src/components/ListDepartmentComponent.jsx
+++ src/components/ListDepartmentComponent.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { deleteDepartment, getAllDepartments } from '../services/DepartmentService';
 import { Link, useNavigate } from 'react-router-dom';
-import './ComponentStyles.css'
+import '../services/ComponentStyles.css';
 
 const ListDepartmentComponent = () => {
 

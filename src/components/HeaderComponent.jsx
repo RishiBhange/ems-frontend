111256c381c8636490091043601cb7f12627f916
RishiBhange <rishibhange2904@gmail.com>
2026-04-12 18:53:23 +0530

    nav-bar change

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -8,21 +8,22 @@ const HeaderComponent = () => {
             <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
                 <a className="navbar-brand" href="https://wwww.javaguides.net">Employee Management System</a>
 
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
                 <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav">
                     <li className="nav-item">
                         <NavLink className='nav-link' to='/employees'>Employees</NavLink>
                     </li>
-
                     <li className="nav-item">
                         <NavLink className='nav-link' to='/departments'>Departments</NavLink>
                     </li>
                   </ul>
               </div>
-
             </nav>
         </header>
-
     </div>
   )
 }

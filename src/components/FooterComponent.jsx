8e222bfacda231b489b382a565f1f1c60a7d37e1
RishiBhange <155911629+RishiBhange@users.noreply.github.com>
2026-04-12 18:15:11 +0530

    Update FooterComponent.jsx

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import './ComponentStyles.css'
 
 const FooterComponent = () => {
-  return (
-    <div>
-        <footer className='footer'>
-            
+    return (
+        <footer className='modern-footer'>
+            <div className='footer-content'>
+                <div className='footer-section'>
+                    <h3 className='footer-title'>Employee Management System</h3>
+                    <p className='footer-text'>Streamline your workforce management with our modern solution</p>
+                </div>
+                <div className='footer-section'>
+                    <p className='footer-copyright'>
+                        © {new Date().getFullYear()} All Rights Reserved
+                    </p>
+                </div>
+            </div>
         </footer>
-
-    </div>
-  )
+    )
 }
 
 export default FooterComponent
